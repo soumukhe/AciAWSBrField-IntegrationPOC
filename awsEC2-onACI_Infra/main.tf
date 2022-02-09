51ed5f3d20bed38b37506ed1862d76481069ca8b
soumukhe <soumukhe@cisco.com>
2022-02-09 14:59:59 -0600

    changed matched value for tenant in aws ec2 for tenant

--- awsEC2-onACI_Infra/main.tf
+++ awsEC2-onACI_Infra/main.tf
@@ -22,7 +22,7 @@ provider "aws" {
 
 data "aws_vpcs" "vpc_id" {
   tags = {
-    AciPolicyDnTag = "*-sm-terraform-*" # adding filter for "uni/tn-sm-terraform-T1/ctxprofile-vrf1-us-east-1"
+    AciPolicyDnTag = "*-aciAwsBrownfieldPOC-*" # adding filter for "uni/tn-sm-terraform-T1/ctxprofile-vrf1-us-east-1"
   }
 }
 
